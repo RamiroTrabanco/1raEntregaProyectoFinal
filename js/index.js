dec98cfe1d1e361e87c676d4bd3572cd3058c3d2
RamiroTrabanco <trabancoramiro@gmail.com>
2022-08-09 17:39:23 -0300

    primer commit

---
+++ js/index.js
@@ -0,0 +1,54 @@
+let verdadero = true
+let falso = false
+let total = 0
+
+const productos = []
+
+class Productos {
+    constructor (nombre, valor, stock){
+        this.nombre=nombre
+        this.valor=valor
+        this.stock=stock
+    }
+}
+
+productos.push(new Productos("3050Palit", 70000, 50))
+productos.push(new Productos("3050Asus", 75000, 50))
+productos.push(new Productos("3060Palit", 90000, 50))
+productos.push(new Productos("3070tiGB", 150000, 50))
+productos.push(new Productos("3070tiPalit", 170000, 50))
+
+
+while (verdadero) {
+    if(confirm("Desea agregar productos al carrito?")) {
+        calcularTotal()
+    } else {
+        break;
+    }
+}
+
+function calcularTotal() {
+    let pedido = prompt("Que producto desea adquirir?")
+
+    const busqueda = productos.find((el) => el.nombre === pedido)
+
+    if (busqueda) {
+        total += busqueda.valor
+    }
+    
+    console.log("El total parcial a pagar es $", total)
+
+    let confirmacion = confirm("Desea agregar más productos al carrito?")
+
+    if (confirmacion) {
+        calcularTotal()
+    } else {
+        console.log("El total a pagar es $", total)
+    }
+}
+
+function buscador() {
+    let buscar = prompt("Ingrese el producto que esta buscando:")
+    const resultadoBusqueda = productos.filter(elemento => elemento.nombre.includes(buscar))
+    console.log(resultadoBusqueda)
+}
